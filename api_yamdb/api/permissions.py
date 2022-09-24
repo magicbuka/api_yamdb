ecfb934e7fc19b60295ff79c8c057864bde18f1e
grachevvladislav <vladislav.grachev@gmail.com>
2022-09-24 22:48:49 +0300

    fix regular expression

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -29,10 +29,10 @@ class IsAdminAuthorModeratorOrReadOnly(permissions.BasePermission):
     """
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in permissions.SAFE_METHODS or (
+            request.method in permissions.SAFE_METHODS or
+            obj.author == request.user or (
                 request.user.is_authenticated and (
-                    obj.author == request.user
-                    or request.user.is_moderator()
+                    request.user.is_moderator()
                     or request.user.is_admin()
                 )
             )
--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -4,7 +4,7 @@ import re
 from rest_framework.exceptions import ValidationError
 
 WRONG_USERNAME = '"me" - недопустимое имя пользователя!'
-WRONG_SYMBOLS = 'Допустимы только буквы, цифры, "@", ".", "+", "-" и "_".'
+WRONG_SYMBOLS = 'Недопустимые символы: {}'
 WRONG_YEAR = (
     'Недопустимое значение поля "Год"! '
     'Указанное значение: {} превышает значение текущего года: {}'
@@ -19,7 +19,11 @@ def username_validator(value):
     if value == 'me':
         raise ValidationError(WRONG_USERNAME)
     if not re.fullmatch(r'^[\w.@+-]+', value):
-        raise ValidationError(WRONG_SYMBOLS)
+        raise ValidationError(
+            WRONG_SYMBOLS.format(', '.join(re.findall(
+                r"""((?![\w.@+-]).)""", value))
+            )
+        )
     return value
 
 
