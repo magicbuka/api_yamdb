42ac430723d2af4192a71e88d92036cfdf514a92
grachevvladislav <vladislav.grachev@gmail.com>
2022-09-25 10:07:59 +0300

    fix regular expression

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -4,7 +4,7 @@ import re
 from rest_framework.exceptions import ValidationError
 
 WRONG_USERNAME = '"me" - недопустимое имя пользователя!'
-WRONG_SYMBOLS = 'Недопустимые символы: {}'
+WRONG_SYMBOLS = "Недопустимые символы: {}"
 WRONG_YEAR = (
     'Недопустимое значение поля "Год"! '
     'Указанное значение: {} превышает значение текущего года: {}'
@@ -20,8 +20,8 @@ def username_validator(value):
         raise ValidationError(WRONG_USERNAME)
     if not re.fullmatch(r'^[\w.@+-]+', value):
         raise ValidationError(
-            WRONG_SYMBOLS.format(', '.join(re.findall(
-                r"""((?![\w.@+-]).)""", value))
+            WRONG_SYMBOLS.format(
+                "".join(set(re.findall(r"[^\w.@+-]", value)))
             )
         )
     return value
